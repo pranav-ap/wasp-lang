54229fea1522805ed22487ad220a50d340ea8272
Pranav <pranavap96@gmail.com>
2020-03-25 15:12:56 +0530

    added int project

---
+++ wasp/wasp.cpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <vector>
+
+#include "io.h"
+#include "Lexer.h"
+#include "Parser.h"
+#include "Interpreter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "\n Wasp Language Interpreter \n" << endl;
+
+	string raw_source = read_source("./../scraps.wasp");
+
+	Lexer lexer(raw_source);
+	vector<Token_ptr> tokens = lexer.execute();
+
+	cout << "\n Scanned Tokens \n" << endl;
+
+	for (auto const& token : tokens)
+		token->print();
+
+	Parser parser(tokens);
+	Module mod = parser.execute();
+	mod.print();
+
+	Interpreter interpreter(mod);
+	interpreter.execute();
+}
